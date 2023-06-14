7f6a0f04ee0f23fb90cced73d98f807257f2f2ce
Ummuhusnul <ummuhusnul124@gmail.com>
2023-06-14 17:08:54 +0700

    First Commit

--- app/src/main/java/com/example/curecornapp/recommend/Drug.kt
+++ app/src/main/java/com/example/curecornapp/recommend/Drug.kt
@@ -1,11 +1,12 @@
-package com.example.curecornapp
+package com.example.curecornapp.recommend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.curecornapp.R
 
-class Treatment : AppCompatActivity() {
+class Drug : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_treatment)
+        setContentView(R.layout.activity_drug)
     }
 }
